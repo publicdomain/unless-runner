d1cc30551714518aeb1f6b4205a66edf1af3c8c5
Victor <publicdomainvic@gmail.com>
2023-03-26 12:04:52 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick()

--- UnlessRunner/MainForm.cs
+++ UnlessRunner/MainForm.cs
@@ -287,7 +287,8 @@ namespace UnlessRunner
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub
+            Process.Start("https://github.com/publicdomain/unless-runner");
         }
 
         /// <summary>
